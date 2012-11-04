2dc35873b2bb92964013e80c03da15ff05760e2a
Ben Woosley <ben.woosley@gmail.com>
2012-11-04 13:33:58 -0800

    Add utf-8 declarations for production.

--- app/models/nation.rb
+++ app/models/nation.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 class Nation < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: :slugged
--- db/migrate/20121104203118_populate_cato_fotw_ratings.rb
+++ db/migrate/20121104203118_populate_cato_fotw_ratings.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 class PopulateCatoFotwRatings < ActiveRecord::Migration
   def up
     Rating.delete_all
