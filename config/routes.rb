aa2e170b043661a52895dc210a2181f58d8f1fdd
Ben Woosley <ben.woosley@gmail.com>
2012-11-03 18:15:58 -0700

    Add a silly landing page.

--- config/routes.rb
+++ config/routes.rb
@@ -46,9 +46,7 @@ NationRank::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
